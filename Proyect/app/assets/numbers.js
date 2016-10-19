0096d427d32dacebfbbbc914241488759a677aeb
JesusViveros <gostrecon@live.com.mx>
2016-10-19 15:17:30 -0500

    Update

---
+++ Proyect/app/assets/numbers.js
@@ -0,0 +1,15 @@
+function isNumberKey(evt){
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+		return false;
+
+	return true;
+}
+
+function isDigitKey(evt){
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57))
+		return false;
+
+	return true;
+}
